32998f59fed5cd3ccdfed9bbb08e1828f4a7df2a
Ethan <thegamershollow@outlook.com>
2024-06-25 18:18:24 +0000

    made changes to test.py

--- test.py
+++ test.py
@@ -3,8 +3,20 @@ import os
 
 # function to download a single youtube video
 def downloadVideo(url):
+    # set yt to use the youtube function from pytube
     yt = YouTube(url)
+    
+    # get the video title
     ytTitle = yt.title
-    print(f"Downloading '{ytTitle}' to {os.getcwd()}")
-    video = yt.streams.get_highest_resolution().download()
-    print(f"Downloaded '{ytTitle}' succesfully!")
+    
+    video = yt.streams.first()
+    print(video)
+    
+    streams = yt.streams
+    x = streams.filter(adaptive=True,progressive=False)
+    #for each in x:
+    #    print(each)
+    # download video
+    #video.download()
+
+downloadVideo("https://m.youtube.com/watch?v=Kq6GO5nNoxM")
