04e2f6d2ae87478601bebd0d8d9bf54bbcfa9af6
Ethan <thegamershollow@outlook.com>
2024-06-25 16:56:40 +0000

    Added playlist download function and terminal gui

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 from pytube import YouTube
+from pytube import Playlist
+import os 
+import sys
 
+def make_alpha_numeric(string):
+    return ''.join(char for char in string if char.isalnum())
+
+# function to download a single youtube video
 def downloadVideo(url):
     yt = YouTube(url)
     ytTitle = yt.title
+    print(f"Downloading '{ytTitle}' to {os.getcwd()}")
     video = yt.streams.get_highest_resolution().download()
     print(f"Downloaded '{ytTitle}' succesfully!")
 
-i = input("Type the url of the video you want to download: ")
-downloadVideo(i)
+# function to download an entire youtube playlist
+def downloadPlaylist(url):
+    ytPlaylist = Playlist(url)
+    folderName = make_alpha_numeric(ytPlaylist.title)
+    os.mkdir(folderName)
+    totalVideoCount = len(ytPlaylist.videos)
+    print("Total videos in playlist: ", totalVideoCount)
+    for index, video in enumerate(ytPlaylist.videos, start=1):
+        print("Downloading:", video.title)
+        video.streams.get_highest_resolution().download(output_path=folderName)
+
+prompt = "YouTube Video/Playlist Downloader\n\n1. Download a video\n2. Download a playlist\n3. Exit"
+if prompt == "1":
+    i = input("Enter the video URL: ")
+    downloadVideo(i)
+if prompt == "2":
+    i = input("Enter the playlist url: ")
+    downloadPlaylist(i)
+if prompt == "3":
+    sys.exit("Exited.")
+if int(prompt) <= 3:
+    print("Not a valid option.")
