b07442ecd5a068f907b1cc525f36c4961fdc9ccc
Ethan <thegamershollow@outlook.com>
2024-06-26 14:29:55 +0000

    fixed download function
    made it so that you can download high res video

--- main.py
+++ main.py
@@ -11,19 +11,28 @@ def downloadVideo(url):
     yt = YouTube(url)
     ytTitle = yt.title
     print(f"Downloading '{ytTitle}' to {os.getcwd()}")
-    video = yt.streams.get_highest_resolution().download()
+    #video = yt.streams.get_highest_resolution().download()
+    video = yt.streams.filter(file_extension='mp4',only_audio=False)
+    dVideo = video[1]
+    videoName = make_alpha_numeric(ytTitle)
+    dVideo.download()
     print(f"Downloaded '{ytTitle}' succesfully!")
 
 # function to download an entire youtube playlist
 def downloadPlaylist(url):
     ytPlaylist = Playlist(url)
+    
     folderName = make_alpha_numeric(ytPlaylist.title)
     os.mkdir(folderName)
     totalVideoCount = len(ytPlaylist.videos)
     print("Total videos in playlist: ", totalVideoCount)
     for index, video in enumerate(ytPlaylist.videos, start=1):
         print("Downloading:", video.title)
-        video.streams.get_highest_resolution().download(output_path=folderName)
+        ytTitle = video.title
+        videos = video.streams.filter(file_extension='mp4',only_audio=False)
+        dVideo = videos[1]
+        videoName = make_alpha_numeric(ytTitle)
+        dVideo.download(output_path=folderName)
     print("Downloaded all videos succesfully.")
 
 prompt = input("YouTube Video/Playlist Downloader\n\n1. Download a video\n2. Download a playlist\n3. Exit\n: ")
