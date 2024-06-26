0b4a681b2617334a2700f1433a3bfa9797dd07e7
Ethan <thegamershollow@outlook.com>
2024-06-26 14:42:45 +0000

    updated test.py

--- test.py
+++ test.py
@@ -1,4 +1,27 @@
 from pytube import YouTube
+import re
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+
 
 url = 'https://m.youtube.com/watch?v=X9SoIejJjwA'
 yt = YouTube(url)
@@ -7,6 +30,7 @@ download = yt.streams.filter(file_extension='mp4',only_audio=False)
 #    print(each)
 dVideo = download[1]
 print(dVideo)
-dVideo.download(filename="video.mp4")
+ytTitle = remove_emojis(yt.title)
+dVideo.download(filename=f"{ytTitle}.mp4")
 #dVideo = download
 #dVideo.download()
